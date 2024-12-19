8e4ee15b368cd416fac3c37720ed43e9f765568d
Jie Wang <wangjie212@amss.ac.cn>
2024-12-19 21:25:39 +0800

    optimize monomial basis

--- src/sdp.jl
+++ src/sdp.jl
@@ -6,21 +6,60 @@ end
 
 cosmo_para() = cosmo_para(1e-5, 1e-5, 1e4)
 
-function beta_number(supp, coe, n, edges, d; QUIET=false, solver="Mosek", cosmo_setting=cosmo_para())
+function beta_number(supp, coe, n, edges, d, s; constraints=nothing, QUIET=false, solver="Mosek", cosmo_setting=cosmo_para())
     println("********************************** BetaNumber **********************************")
     println("BetaNumber is launching...")
-    basis = get_mbasis(n, min(n, d))
-    tbasis = get_mbasis(n, min(n, 2d))
+    # mbasis = get_mbasis(n, d)
+    # tbasis = get_mbasis(n, min(n, 2d))
+    mbasis = get_mbasis(n, n)
+    tbasis = get_mbasis(n, n)
     sort!(tbasis)
-    ind = [bfind(tbasis, length(tbasis), [i]) for i = 1:n]
-    lb = length(basis)
+    ind = [bfind(tbasis, length(tbasis), mbasis[i]) for i = 1:length(mbasis)]
+    ind0 = [bfind(tbasis, length(tbasis), [i]) for i = 1:n]
+    # temp = [[UInt8[1]]; get_sbasis(Vector(2:sum(length.(mbasis).<=s)), d)[2:end]]
+    temp = [[UInt8[1]]; get_tbasis(Vector(2:sum(length.(mbasis).<=s)), d)]
+    temp = temp[[sum(length.(mbasis[temp[i]])) for i = 1:length(temp)] .<= d]
+    basis = Vector{Vector{Vector{Vector{UInt8}}}}(undef, 2)
+    basis[1] = Vector{Vector{UInt8}}[]
+    basis[2] = Vector{Vector{UInt8}}[]
+    for i = 1:length(temp), j = 1:length(mbasis)
+        l = sum(length.(mbasis[temp[i]])) + length(mbasis[j])
+        w = vcat(mbasis[temp[i]]...)
+        if l <= d && reduce(w) == mbasis[j] && sum(length.(mbasis[temp[i]])) != 6 && (sum(length.(mbasis[temp[i]])) == length(reduce(w)) || l < 6) && length(temp[i]) != 3 && (l != 4 || length(mbasis[j]) != 2)
+        # if 2*length(mbasis[j]) <= d && reduce(w) == mbasis[j]
+            # if iseven(l)
+                push!(basis[1], [temp[i], [j]])
+            # else
+            #     push!(basis[2], [temp[i], [j]])
+            # end
+        end
+    end
+    # for j = 1:length(mbasis)
+    #     if 2*length(mbasis[j]) > d
+    #         push!(basis[1], [mbasis[j].+1, [j]])
+    #     end
+    # end
     tsupp = Vector{UInt16}[]
-    for i = 1:lb, j = i:lb
-        word,v1 = reduce(basis[i], basis[j], edges)
-        v2 = reduce(basis[j], basis[i], edges)[2]
+    lb = length.(basis)
+    println(lb)
+    for k = 1:1, i = 1:lb[k], j = i:lb[k]
+        word,v1 = reduce(mbasis[basis[k][i][2][1]], mbasis[basis[k][j][2][1]], edges)
+        v2 = reduce(mbasis[basis[k][j][2][1]], mbasis[basis[k][i][2][1]], edges)[2]
         if v1 == v2
             a = bfind(tbasis, length(tbasis), word)
-            word = a == 1 ? sort([ind[basis[i]]; ind[basis[j]]]) : sort([ind[basis[i]]; ind[basis[j]]; a])
+            if a == 1 && basis[k][i][1] == [1] && basis[k][j][1] == [1]
+                word = [1]
+            elseif a == 1 && basis[k][i][1] == [1]
+                word = ind[basis[k][j][1]]
+            elseif a == 1
+                word = sort([ind[basis[k][i][1]]; ind[basis[k][j][1]]])
+            elseif basis[k][i][1] == [1] && basis[k][j][1] == [1]
+                word = [a]
+            elseif basis[k][i][1] == [1]
+                word = sort([ind[basis[k][j][1]]; a])
+            else
+                word = sort([ind[basis[k][i][1]]; ind[basis[k][j][1]]; a])
+            end
             push!(tsupp, word)
         end
     end
@@ -34,13 +73,26 @@ function beta_number(supp, coe, n, edges, d; QUIET=false, solver="Mosek", cosmo_
     end
     set_optimizer_attribute(model, MOI.Silent(), QUIET)
     cons = [AffExpr(0) for i=1:ltsupp]
-    pos = @variable(model, [1:lb, 1:lb], PSD)
-    for i = 1:lb, j = i:lb
-        word,v1 = reduce(basis[i], basis[j], edges)
-        v2 = reduce(basis[j], basis[i], edges)[2]
+    for k = 1:1
+        pos = @variable(model, [1:lb[k], 1:lb[k]], PSD)
+        for i = 1:lb[k], j = i:lb[k]
+        word,v1 = reduce(mbasis[basis[k][i][2][1]], mbasis[basis[k][j][2][1]], edges)
+        v2 = reduce(mbasis[basis[k][j][2][1]], mbasis[basis[k][i][2][1]], edges)[2]
         if v1 == v2
             a = bfind(tbasis, length(tbasis), word)
-            word = a == 1 ? sort([ind[basis[i]]; ind[basis[j]]]) : sort([ind[basis[i]]; ind[basis[j]]; a])
+            if a == 1 && basis[k][i][1] == [1] && basis[k][j][1] == [1]
+                word = [1]
+            elseif a == 1 && basis[k][i][1] == [1]
+                word = ind[basis[k][j][1]]
+            elseif a == 1
+                word = sort([ind[basis[k][i][1]]; ind[basis[k][j][1]]])
+            elseif basis[k][i][1] == [1] && basis[k][j][1] == [1]
+                word = [a]
+            elseif basis[k][i][1] == [1]
+                word = sort([ind[basis[k][j][1]]; a])
+            else
+                word = sort([ind[basis[k][i][1]]; ind[basis[k][j][1]]; a])
+            end
             loc = bfind(tsupp, ltsupp, word)
             if i == j
                 @inbounds add_to_expression!(cons[loc], v1, pos[i,i])
@@ -49,14 +101,25 @@ function beta_number(supp, coe, n, edges, d; QUIET=false, solver="Mosek", cosmo_
             end
         end
     end
+    end
     bc = zeros(ltsupp)
     for i = 1:length(supp)
-        Locb = bfind(tsupp, ltsupp, ind[supp[i]])
+        Locb = bfind(tsupp, ltsupp, ind0[supp[i]])
         bc[Locb] = -coe[i]
     end
     @variable(model, up)
     cons[1] -= up
-    @constraint(model, cons.==bc)
+    Locb = [bfind(tsupp, ltsupp, ind0[item]) for item in supp]
+    if constraints !== nothing
+        z = @variable(model, [1:length(constraints)], lower_bound=0)
+        for i = 1:length(constraints)
+            cons[1] += constraints[i][2]*z[i]
+            for j = 1:length(supp)
+                @inbounds add_to_expression!(cons[Locb[j]], -constraints[i][1][j], z[i])
+            end
+        end
+    end
+    @constraint(model, con[i=1:ltsupp], cons[i]==bc[i])
     @objective(model, Min, up)
     if QUIET == false
         println("Solving the SDP...")
@@ -70,5 +133,20 @@ function beta_number(supp, coe, n, edges, d; QUIET=false, solver="Mosek", cosmo_
        println("solution status: $status")
     end
     println("optimum = $objv")
-    return objv
+    dual_var = -dual.(con)
+    return objv,dual_var[Locb]
+end
+
+function reduce(w)
+    w = sort(w)
+    i = 1
+    while i < length(w)
+        if w[i] == w[i+1]
+            deleteat!(w, i)
+            deleteat!(w, i)
+        else
+            i += 1
+        end
+    end
+    return w
 end
--- src/utils.jl
+++ src/utils.jl
@@ -8,6 +8,27 @@ function get_mbasis(n, d)
     return basis
 end
 
+function get_tbasis(var, d)
+    basis = [UInt8[i] for i in var]
+    nbasis = [UInt8[i] for i in var]
+    for i = 2:d
+        nbasis = lift_tbasis(nbasis, var)
+        append!(basis, nbasis)
+    end
+    return basis
+end
+
+function lift_tbasis(basis, var)
+    nbasis = Vector{UInt8}[]
+    for item in basis
+        if item[end] < var[end]
+            k = bfind(var, length(var), item[end])
+            append!(nbasis, [[item; j] for j in var[k+1:end]])
+        end
+    end
+    return nbasis
+end
+
 function lift_basis(basis, n)
     nbasis = Vector{UInt8}[]
     for item in basis
@@ -16,6 +37,36 @@ function lift_basis(basis, n)
         end
     end
     return nbasis
+end
+
+function get_sbasis(var, d)
+    n = length(var)
+    lb = binomial(n+d, d)
+    basis = Vector{Vector{UInt16}}(undef, lb)
+    basis[1] = UInt16[]
+    i = 0
+    t = 1
+    while i < d+1
+        t += 1
+        if sum(basis[t-1]) == var[n]*i
+           if i < d
+               basis[t] = var[1]*ones(UInt16, i+1)
+           end
+           i += 1
+        else
+            j = bfind(var, n, basis[t-1][1])
+            basis[t] = copy(basis[t-1])
+            ind = findfirst(x->basis[t][x]!=var[j], 1:length(basis[t]))
+            if ind === nothing
+                ind = length(basis[t])+1
+            end
+            if j != 1
+                basis[t][1:ind-2] = var[1]*ones(UInt16, ind-2)
+            end
+            basis[t][ind-1] = var[j+1]
+        end
+    end
+    return basis
 end
 
 function bfind(A, l, a)

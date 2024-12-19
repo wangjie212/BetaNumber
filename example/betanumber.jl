4fb6d3b0fb1be77b5a99bdac9c090ca49ad36ad1
Jie Wang <wangjie212@amss.ac.cn>
2024-12-20 00:10:18 +0800

    improve codes

--- example/betanumber.jl
+++ example/betanumber.jl
@@ -3,15 +3,15 @@ using BetaNumber
 
 ## graph6 code to igraph
 # nx = pyimport("networkx")
-ig6 = "GUZv\\{"
-edges = [[i+1,j+1] for (i,j) in nx.from_graph6_bytes(codeunits(ig6)).edges()]
+# ig6 = "GUZv\\{"
+# edges = [[i+1,j+1] for (i,j) in nx.from_graph6_bytes(codeunits(ig6)).edges()]
 # edges = [[1,2], [1,4], [1,5], [1,7], [2,3], [2,4], [2,5], [3,4], [3,6], [4,5], [4,7], [5,6], [5,7], [6,7]]
-# edges = [[1,2], [2,3], [3,4], [4,5], [1,5], [1,6], [2,6], [3,6], [4,6], [5,6]]
+edges = [[1,2], [2,3], [3,4], [4,5], [1,5], [1,6], [2,6], [3,6], [4,6], [5,6]]
 # edges = [[1, 3], [1, 5], [1, 6], [1, 7], [2, 4], [2, 5], [2, 7], [3, 5], [3, 6], [3, 7], [4, 6], [4, 7], [5, 7], [6, 7]]
 sort!(edges)
-n = 8
+n = 6
 supp = [[i;i] for i = 1:n]
-# coe = [ones(6); 2]
+coe = [ones(5); 2]
 # coe = ones(n)
-coe = [1, 1, 1, 1, 1, 1, 1, 1]
+# coe = [1, 1, 1, 1, 1, 1, 1, 1]
 beta_number(supp, coe, n, edges, QUIET=false)
--- src/sdp.jl
+++ src/sdp.jl
@@ -1,40 +1,41 @@
 function beta_number(supp, coe, n, edges; QUIET=false)
     println("********************************** BetaNumber **********************************")
     println("BetaNumber is launching...")
-    mbasis = get_mbasis(n, 3)
     tbasis = get_mbasis(n, 6)
     sort!(tbasis)
-    ind = [bfind(tbasis, length(tbasis), mbasis[i]) for i = 1:length(mbasis)]
-    ind0 = [bfind(tbasis, length(tbasis), [i]) for i = 1:n]
-    temp = [[UInt8[1]]; get_tbasis(Vector(2:sum(length.(mbasis).<=2)), 2)]
-    temp = temp[[sum(length.(mbasis[temp[i]])) for i = 1:length(temp)] .<= 3]
-    basis = Vector{Vector{UInt8}}[]
-    for i = 1:length(temp), j = 1:length(mbasis)
-        l = sum(length.(mbasis[temp[i]])) + length(mbasis[j])
-        w = vcat(mbasis[temp[i]]...)
-        if l <= 6 && reduce(w) == mbasis[j] && sum(length.(mbasis[temp[i]])) != 6 && (sum(length.(mbasis[temp[i]])) == length(reduce(w)) || l < 6) && length(temp[i]) != 3 && (l != 4 || length(mbasis[j]) != 2)
-            push!(basis, [temp[i], [j]])
-        end
+    ind = [bfind(tbasis, length(tbasis), [i]) for i = 1:n]
+    basis = [[UInt8[1], UInt8[1]]]
+    for i = 1:n
+        push!(basis, [[ind[i]], [ind[i]]])
+    end
+    for i = 1:n-1, j = i+1:n
+        a = bfind(tbasis, length(tbasis), [i;j])
+        push!(basis, [[ind[i]; a], [ind[i]]], [[ind[i]; a], [ind[j]]])
+    end
+    for i = 1:n-1, j = i+1:n, k in setdiff(Vector(1:n), [i;j])
+        a = bfind(tbasis, length(tbasis), [i;j])
+        b = bfind(tbasis, length(tbasis), sort([i;j;k]))
+        push!(basis, [sort([a; ind[k]]), [b]])
     end
     tsupp = Vector{UInt16}[]
     lb = length(basis)
     for i = 1:lb, j = i:lb
-        word,v1 = reduce(mbasis[basis[i][2][1]], mbasis[basis[j][2][1]], edges)
-        v2 = reduce(mbasis[basis[j][2][1]], mbasis[basis[i][2][1]], edges)[2]
+        word,v1 = reduce(tbasis[basis[i][2][1]], tbasis[basis[j][2][1]], edges)
+        v2 = reduce(tbasis[basis[j][2][1]], tbasis[basis[i][2][1]], edges)[2]
         if v1 == v2
             a = bfind(tbasis, length(tbasis), word)
             if a == 1 && basis[i][1] == [1] && basis[j][1] == [1]
                 word = [1]
             elseif a == 1 && basis[i][1] == [1]
-                word = ind[basis[j][1]]
+                word = basis[j][1]
             elseif a == 1
-                word = sort([ind[basis[i][1]]; ind[basis[j][1]]])
+                word = sort([basis[i][1]; basis[j][1]])
             elseif basis[i][1] == [1] && basis[j][1] == [1]
                 word = [a]
             elseif basis[i][1] == [1]
-                word = sort([ind[basis[j][1]]; a])
+                word = sort([basis[j][1]; a])
             else
-                word = sort([ind[basis[i][1]]; ind[basis[j][1]]; a])
+                word = sort([basis[i][1]; basis[j][1]; a])
             end
             push!(tsupp, word)
         end
@@ -48,22 +49,22 @@ function beta_number(supp, coe, n, edges; QUIET=false)
     cons = [AffExpr(0) for i=1:ltsupp]
     pos = @variable(model, [1:lb, 1:lb], PSD)
     for i = 1:lb, j = i:lb
-        word,v1 = reduce(mbasis[basis[i][2][1]], mbasis[basis[j][2][1]], edges)
-        v2 = reduce(mbasis[basis[j][2][1]], mbasis[basis[i][2][1]], edges)[2]
+        word,v1 = reduce(tbasis[basis[i][2][1]], tbasis[basis[j][2][1]], edges)
+        v2 = reduce(tbasis[basis[j][2][1]], tbasis[basis[i][2][1]], edges)[2]
         if v1 == v2
             a = bfind(tbasis, length(tbasis), word)
             if a == 1 && basis[i][1] == [1] && basis[j][1] == [1]
                 word = [1]
             elseif a == 1 && basis[i][1] == [1]
-                word = ind[basis[j][1]]
+                word = basis[j][1]
             elseif a == 1
-                word = sort([ind[basis[i][1]]; ind[basis[j][1]]])
+                word = sort([basis[i][1]; basis[j][1]])
             elseif basis[i][1] == [1] && basis[j][1] == [1]
                 word = [a]
             elseif basis[i][1] == [1]
-                word = sort([ind[basis[j][1]]; a])
+                word = sort([basis[j][1]; a])
             else
-                word = sort([ind[basis[i][1]]; ind[basis[j][1]]; a])
+                word = sort([basis[i][1]; basis[j][1]; a])
             end
             loc = bfind(tsupp, ltsupp, word)
             if i == j
@@ -75,12 +76,11 @@ function beta_number(supp, coe, n, edges; QUIET=false)
     end
     bc = zeros(ltsupp)
     for i = 1:length(supp)
-        Locb = bfind(tsupp, ltsupp, ind0[supp[i]])
+        Locb = bfind(tsupp, ltsupp, ind[supp[i]])
         bc[Locb] = -coe[i]
     end
     @variable(model, up)
     cons[1] -= up
-    Locb = [bfind(tsupp, ltsupp, ind0[item]) for item in supp]
     @constraint(model, con[i=1:ltsupp], cons[i]==bc[i])
     @objective(model, Min, up)
     if QUIET == false
@@ -95,6 +95,6 @@ function beta_number(supp, coe, n, edges; QUIET=false)
        println("solution status: $status")
     end
     println("optimum = $objv")
-    dual_var = -dual.(con)
-    return objv,dual_var[Locb]
+    # dual_var = -dual.(con)
+    return objv
 end
